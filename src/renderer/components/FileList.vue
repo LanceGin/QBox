6f8d0c8c5ba006c5ad46ff29974b8b0800c9080b
lancegin <gin.lance.inside@hotmail.com>
2017-07-27 19:44:52 +0800

    add: FileList component

---
+++ src/renderer/components/FileList.vue
@@ -0,0 +1,108 @@
+<template>
+  <div id="file-list-page">
+    <el-table
+      ref="multipleTable"
+      :data="tableData3"
+      tooltip-effect="dark"
+      style="width: 100%"
+      @selection-change="handleSelectionChange">
+      <el-table-column
+        type="selection"
+        width="55">
+      </el-table-column>
+      <el-table-column
+        prop="key"
+        label="文件名"
+        width="300">
+      </el-table-column>
+      <el-table-column
+        prop="mType"
+        label="文件类型"
+        width="150">
+      </el-table-column>
+      <el-table-column
+        prop="fsize"
+        label="文件大小"
+        width="150">
+      </el-table-column>
+      <el-table-column
+        prop="putTime"
+        label="更新时间"
+        width="200"
+        show->
+      </el-table-column>
+      <el-table-column
+        label="操作"
+        width="145">
+        <template scope="scope">
+          <el-button type="text" size="small" icon="view"></el-button>
+          <el-button type="text" size="small">删除</el-button>
+          <el-button type="text" size="small">替换</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'file-list',
+    data() {
+      return {
+        tableData3: [{
+          key: 'assets/Home-Slider/slide-1.jpg',
+          mType: 'image/jpeg',
+          fsize: '640 KB',
+          putTime: '2016-05-03',
+        }, {
+          key: 'assets/Home-Slider/slide-1.jpg',
+          mType: 'image/jpeg',
+          fsize: '640 KB',
+          putTime: '2016-05-03',
+        }, {
+          key: 'assets/Home-Slider/slide-1.jpg',
+          mType: 'image/jpeg',
+          fsize: '640 KB',
+          putTime: '2016-05-03',
+        }, {
+          key: 'assets/Home-Slider/slide-1.jpg',
+          mType: 'image/jpeg',
+          fsize: '640 KB',
+          putTime: '2016-05-03',
+        }, {
+          key: 'assets/Home-Slider/slide-1.jpg',
+          mType: 'image/jpeg',
+          fsize: '640 KB',
+          putTime: '2016-05-03',
+        }, {
+          key: 'assets/Home-Slider/slide-1.jpg',
+          mType: 'image/jpeg',
+          fsize: '640 KB',
+          putTime: '2016-05-03',
+        }],
+        multipleSelection: [],
+      };
+    },
+    methods: {
+      toggleSelection(rows) {
+        if (rows) {
+          rows.forEach((row) => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      },
+    },
+  };
+</script>
+
+<style scope>
+  #file-list-page {
+    max-height: 590px;
+    overflow: scroll;
+  }
+</style>
--- src/renderer/pages/Manage.vue
+++ src/renderer/pages/Manage.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="manage-page">
     <bucket-header></bucket-header>
+    <div class="file-list">
+      <file-list></file-list>
+    </div>
   </div>
 </template>
 
 <script>
   import BucketHeader from '../components/BucketHeader';
+  import FileList from '../components/FileList';
 
   export default {
     name: 'manage',
-    components: { BucketHeader },
+    components: { BucketHeader, FileList },
   };
 </script>
 
@@ -21,5 +25,8 @@
   body {
     margin: 0;
     font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  }
+  .file-list {
+    padding-top: 50px;
   }
 </style>
