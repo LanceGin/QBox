2de0531aa23f8ce363e6c246a0ffb464cc625ccb
lancegin <gin.lance.inside@hotmail.com>
2017-07-28 09:56:31 +0800

    upd: update FileList style

--- src/renderer/components/FileList.vue
+++ src/renderer/components/FileList.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="file-list-page">
+    <div class="manage-btn-group">
+      <el-button class="upload-btn" type="text" icon="upload">upload</el-button>
+      <el-button class="refresh-btn" type="text" icon="time">refresh</el-button>
+    </div>
     <el-table
       ref="multipleTable"
       :data="tableData3"
@@ -8,12 +12,12 @@
       @selection-change="handleSelectionChange">
       <el-table-column
         type="selection"
-        width="55">
+        width="35">
       </el-table-column>
       <el-table-column
         prop="key"
         label="文件名"
-        width="300">
+        width="320">
       </el-table-column>
       <el-table-column
         prop="mType"
@@ -104,5 +108,64 @@
   #file-list-page {
     max-height: 590px;
     overflow: scroll;
+  }
+  .upload-btn {
+    position: fixed;
+    left: 160px;
+    top: 6px;
+    background: transparent;
+    color: #fff;
+    font-size: 18px;
+  }
+  .upload-btn:hover,
+  .upload-btn:focus {
+    color: #fff;
+  }
+  .refresh-btn {
+    position: fixed;
+    right: 40px;
+    top: 6px;
+    background: transparent;
+    color: #fff;
+    font-size: 18px;
+  }
+  .refresh-btn:hover,
+  .refresh-btn:focus {
+    color: #fff;
+    cursor: pointer;
+  }
+  /* set table style */
+  .el-table {
+    color: #888;
+  }
+  .el-table__header-wrapper thead div {
+    background: #fff;
+    color: #888;
+    font-size: 14px;
+    font-weight: lighter;
+  }
+  .el-table__header-wrapper th {
+    height: 30px;
+  }
+  .el-checkbox__inner {
+    width: 14px;
+    height: 14px;
+  }
+  .el-checkbox__inner::after {
+    width: 2px;
+    height: 6px;
+  }
+  .el-table th {
+    background: #fff;
+  }
+  .el-table .el-button--text {
+    color: #2e84c7;
+  }
+  .el-table .el-button--text:hover {
+    color: #2e84c7;
+  }
+  .el-table::after,
+  .el-table::before {
+    background: #fff;
   }
 </style>
